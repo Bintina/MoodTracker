c36d1f03d082fad9faf5b60e7855f84f7545e427
Bintina <105106238+Bintina@users.noreply.github.com>
2022-11-22 05:58:15 +0300

    Created data, application and receiver classes
    Created MyApp Application class file, Mood data classs, SaveReceiver Receiver class and Extension file to put methods in.

---
+++ app/src/main/java/com/example/moodtracker/MyApp.kt
@@ -0,0 +1,10 @@
+package com.example.moodtracker
+
+import android.app.Application
+
+class MyApp: Application() {
+
+    companion object{
+
+    }
+}
---
+++ app/src/main/java/com/example/moodtracker/SaveReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.moodtracker
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SaveReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+}
